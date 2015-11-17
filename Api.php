2213b32a22f0cb8e29ad03d4b58a60a5304244b5
Björn Roland <bjoern.roland@intradesys.com>
2015-11-18 00:01:58 +0100

    Just for debugging..

---
+++ Api.php
@@ -0,0 +1,25 @@
+<?php
+require_once('include_me.php');
+
+use SSLLabsApi\Calls\AnalyzeCall;
+use SSLLabsApi\Calls\GetEndpointDataCall;
+
+/*
+//Analyze Call
+$parameters = array('host' => 'ssllabs.com');
+$analyzeCall = new AnalyzeCall($parameters);
+$analyzeCall->setDevMode(true);
+
+echo '<pre>';
+print_r($analyzeCall->send());
+echo '</pre>';
+*/
+
+//getEndpointData Call
+$parameters = array('host' => 'ssllabs.com', 's' => '64.41.200.100');
+$getEndpointDataCall = new GetEndpointDataCall($parameters);
+$getEndpointDataCall->setDevMode(true);
+
+echo '<pre>';
+print_r($getEndpointDataCall->send());
+echo '</pre>';
